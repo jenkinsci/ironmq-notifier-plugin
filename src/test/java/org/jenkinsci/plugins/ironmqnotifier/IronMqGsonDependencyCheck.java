1df43dfaa93eb77e3d75b79da0d5154efe48a2b1
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-03 23:32:14 -0400

    Create test to force dependency check  Will cause compiler error instead of having the plugin fail during run-time

--- src/test/java/org/jenkinsci/plugins/ironmqnotifier/IronMqGsonDependencyCheck.java
+++ src/test/java/org/jenkinsci/plugins/ironmqnotifier/IronMqGsonDependencyCheck.java
@@ -1,22 +1,26 @@
 package org.jenkinsci.plugins.ironmqnotifier;
 
 
+import com.google.gson.Gson;
 import io.iron.ironmq.Message;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IronMqGsonDependencyCheck {
 
-         @Test
-    public void GsonExistsForMessageClassTest()
-         {
+    @Test
+    public void GsonExistsForMessageClassTest() {
 
-             Message msg = new Message();
-             long expiresIn = msg.getExpiresIn();
-             Assert.assertNotNull(expiresIn);
+        Message msg = new Message();
+        long expiresIn = msg.getExpiresIn();
+        Assert.assertNotNull(expiresIn);
+    }
 
-         }
-
+    @Test
+    public void GSON_Test_To_Cause_Compiler_Warning_If_Dependency_Is_Missing()
+    {
+        Gson gson = new Gson();
+        Assert.assertNotNull(gson);
+    }
 
 }
