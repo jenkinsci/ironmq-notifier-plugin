1af9005f46804fd225c9c2bf186cd12f94df7c72
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-08 15:59:03 -0400

    remove implemented message as this does not follow convention for plugins

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronPluginImplement.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronPluginImplement.java
@@ -11,7 +11,7 @@ public class IronPluginImplement extends Plugin {
     private final static Logger LOG = Logger.getLogger(IronPluginImplement.class.getName());
 
     public void start() throws Exception {
-        LOG.info("starting ironmq-notifier plugin");
+        //LOG.info("starting ironmq-notifier plugin");
     }
 }
 
