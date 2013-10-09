b828829ffc5c975154529801ef624ea48d2b527d
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-09 00:45:17 -0400

    minor reformat

--- src/test/java/org/jenkinsci/plugins/ironmqnotifier/IronPluginImplementerTest.java
+++ src/test/java/org/jenkinsci/plugins/ironmqnotifier/IronPluginImplementerTest.java
@@ -1,15 +1,12 @@
 package org.jenkinsci.plugins.ironmqnotifier;
 
-
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class IronPluginImplementerTest {
 
     @Test
-    public void There_Should_Be_A_Plugin_Implementer_Extension()
-    {
+    public void There_Should_Be_A_Plugin_Implementer_Extension() {
 
         IronPluginImplement implement = new IronPluginImplement();
         Assert.assertNotNull(implement);
