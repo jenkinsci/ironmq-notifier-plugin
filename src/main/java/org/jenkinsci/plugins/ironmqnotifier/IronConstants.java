edca20e6e3b0fb6fb9e60012ec20073b19cd3cee
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-15 17:36:20 -0400

    constants added

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
@@ -5,5 +5,4 @@ public class IronConstants {
     public static String DEFAULT_QUEUE_NAME = "Jenkins";
     public static String DEFAULT_PREFERRED_SERVER_NAME = "mq-rackspace-ord.iron.io";
     public static Long DEFAULT_EXPIRY_SECONDS = 806400L;
-
 }
