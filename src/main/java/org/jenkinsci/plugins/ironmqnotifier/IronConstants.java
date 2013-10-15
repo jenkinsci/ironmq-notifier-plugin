73eb1e782cf2f308b25d9a09dad34378fcf772d2
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-15 17:27:36 -0400

    create some constants to not change defaults in too many places

---
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
@@ -0,0 +1,9 @@
+package org.jenkinsci.plugins.ironmqnotifier;
+
+public class IronConstants {
+
+    public static String DEFAULT_QUEUE_NAME = "Jenkins";
+    public static String DEFAULT_PREFERRED_SERVER_NAME = "mq-rackspace-ord.iron.io";
+    public static Long DEFAULT_EXPIRY_SECONDS = 806400L;
+
+}
