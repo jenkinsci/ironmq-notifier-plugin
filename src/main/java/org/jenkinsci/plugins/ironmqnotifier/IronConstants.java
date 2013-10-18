c90280dfcdafff475f7b4c1ae2e16a6b40636dd0
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-18 16:40:15 -0400

    checkstyle

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
@@ -3,11 +3,13 @@ package org.jenkinsci.plugins.ironmqnotifier;
 final class IronConstants {
 
 
-    static String DEFAULT_QUEUE_NAME = "Jenkins";
-    static String DEFAULT_PREFERRED_SERVER_NAME = "mq-rackspace-ord.iron.io";
-    static final int DEFAULT_PREFERRED_SERVER_PORT = 443;
-    static long DEFAULT_EXPIRY_SECONDS = 806400l;  //that's an l not a 1!
-    static long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
+    protected static String DEFAULT_QUEUE_NAME = "Jenkins";
+    protected static String DEFAULT_PREFERRED_SERVER_NAME
+            = "mq-rackspace-ord.iron.io";
+    protected static final int DEFAULT_PREFERRED_SERVER_PORT = 443;
+    protected static long DEFAULT_EXPIRY_SECONDS = 806400l; //(l)ong
+
+    protected static long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
 
     /**
      * <p>Constructor for IronConstants.</p>
