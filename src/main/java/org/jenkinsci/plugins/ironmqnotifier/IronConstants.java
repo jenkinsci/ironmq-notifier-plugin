70b953139a2311fd1262152f0381ee656e123eae
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-17 20:15:52 -0400

    see if we can set constants non-public

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/IronConstants.java
@@ -3,11 +3,11 @@ package org.jenkinsci.plugins.ironmqnotifier;
 public class IronConstants {
 
 
-    public static String DEFAULT_QUEUE_NAME = "Jenkins";
-    public static String DEFAULT_PREFERRED_SERVER_NAME = "mq-rackspace-ord.iron.io";
-    public static final int DEFAULT_PREFERRED_SERVER_PORT = 443;
-    public static long DEFAULT_EXPIRY_SECONDS = 806400L;
-    public static long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
+    static String DEFAULT_QUEUE_NAME = "Jenkins";
+    static String DEFAULT_PREFERRED_SERVER_NAME = "mq-rackspace-ord.iron.io";
+    static final int DEFAULT_PREFERRED_SERVER_PORT = 443;
+    static long DEFAULT_EXPIRY_SECONDS = 806400L;
+    static long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
 
     public IronConstants() {
         //placeholder
