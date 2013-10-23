7bbc8202ff2748145a3d0a3476b135d495859810
Mike Caspar <MikeCaspar@users.noreply.github.com>
2013-10-23 09:49:48 -0400

    sender null queue checks

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronMQSender.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronMQSender.java
@@ -1,45 +1,69 @@
 package org.jenkinsci.plugins.ironmqnotifier.Iron;
 
 
+
 import io.iron.ironmq.Client;
 import io.iron.ironmq.Message;
 import io.iron.ironmq.Queue;
 
 import java.io.IOException;
 
-public class IronMQSender{
+public class IronMQSender {
 
-         public void Send(final Client client,
-                               final MessageSettings messageSettings)
+    public IronMQSender () {
+        // placeholder for testing
+    }
 
-            throws Exception {
+    public void Send ( final Client client,
+                       final MessageSettings messageSettings )
+            throws IOException, IllegalArgumentException {
 
-        Queue queue = client.queue(messageSettings.getQueueName());
+        String test = messageSettings.getQueueName ();
+
+        checkMessageParameters(messageSettings);
+
+        Queue queue = client.queue (messageSettings.getQueueName ());
 
         Message message = new Message();
 
-        IronMQMessage ironMQMessage = new IronMQMessage();
+        IronMQMessage ironMQMessage = new IronMQMessage ();
 
-        ironMQMessage.setBuildResult(messageSettings.getBuildResultString());
-        ironMQMessage.setJobName(messageSettings.getJobName());
+        ironMQMessage.setBuildResult (messageSettings.getBuildResultString ());
+        ironMQMessage.setJobName (messageSettings.getJobName ());
 
-        ironMQMessage.setExpirySeconds(messageSettings.getExpirySeconds());
+        ironMQMessage.setExpirySeconds (messageSettings.getExpirySeconds ());
 
 
-        message.setBody(ironMQMessage.toJson());
+        message.setBody (ironMQMessage.toJson ());
 
-        message.setExpiresIn(messageSettings.getExpirySeconds());
-        message.setBody(message.getBody());
+        message.setExpiresIn (messageSettings.getExpirySeconds ());
+        message.setBody (message.getBody ());
 
 
         String resultOfQueuePush
-                = queue.push(message.getBody(), 0, 0, message.getExpiresIn());
+                = queue.push (message.getBody (), 0, 0, message.getExpiresIn ());
 
         if(resultOfQueuePush == null
-                || resultOfQueuePush.length() == 0) {
-
-
-            throw new IOException("Not successful in sending message");
+                || resultOfQueuePush.length () == 0) {
+            throw new IOException ("Not successful in sending message");
         }
     }
+
+    private void checkMessageParameters (final MessageSettings messageSettings)
+            throws IllegalArgumentException {
+
+        if  (messageSettings.getExpirySeconds() == 0 ) {
+            throw new IllegalArgumentException ("expiry seconds Zero exception");
+        }
+
+        String queueString = messageSettings.getQueueName();
+
+        if (queueString == null || queueString.length () == 0 )
+        {
+            throw new IllegalArgumentException ("queueName is invalid exception");
+        }
+
+
+    }
+
 }
--- src/test/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronMQSenderTest.java
+++ src/test/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronMQSenderTest.java
@@ -1,9 +1,16 @@
 package org.jenkinsci.plugins.ironmqnotifier.Iron;
 
 
+
+import hudson.util.FormValidation;
 import org.jenkinsci.plugins.ironmqnotifier.Iron.IronMQSender;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class IronMQSenderTest{
 
@@ -15,4 +22,49 @@ public class IronMQSenderTest{
         Assert.assertNotNull(sender);
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void SenderThrows_An_Arg_Exception_On_Zero_Expiry()
+            throws IOException, IllegalArgumentException {
+
+        ClientWrapper mockClient = mock(ClientWrapper.class);
+        MessageSettings mockMessageSettings = mock(MessageSettings.class);
+        when(mockMessageSettings.getExpirySeconds ()).thenReturn(0L);
+
+        IronMQSender sender =  new IronMQSender();
+
+        sender.Send (mockClient, mockMessageSettings);
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void SenderThrows_An_Arg_Exception_On_Empty_QueueName()
+            throws IOException, IllegalArgumentException {
+
+        ClientWrapper mockClient = mock(ClientWrapper.class);
+        MessageSettings mockMessageSettings = mock(MessageSettings.class);
+        when(mockMessageSettings.getExpirySeconds ()).thenReturn(1000L); //avoid exception
+        when(mockMessageSettings.getQueueName ()).thenReturn ("");
+
+        IronMQSender sender =  new IronMQSender();
+
+        sender.Send (mockClient, mockMessageSettings);
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void SenderThrows_An_Arg_Exception_On_Null_QueueName()
+            throws IOException, IllegalArgumentException {
+
+        ClientWrapper mockClient = mock(ClientWrapper.class);
+        MessageSettings mockMessageSettings = mock(MessageSettings.class);
+        when(mockMessageSettings.getExpirySeconds ()).thenReturn(1000L); //avoid exception
+        when(mockMessageSettings.getQueueName ()).thenReturn (null);
+
+        IronMQSender sender =  new IronMQSender();
+
+        sender.Send (mockClient, mockMessageSettings);
+
+    }
 }
+
+
