a8bf7aad696efc2aa815d7fdda508d25c2927dd8
Mike Caspar <mike@caspar.com>
2015-03-28 10:42:51 -0400

    adjustment to local test script to use settings.xml from current location

--- localdebug.sh
+++ localdebug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export MAVEN_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
-mvn clean compile hpi:run
+mvn clean --settings ./settings.xml compile hpi:run
 
