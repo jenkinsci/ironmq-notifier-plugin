73d1af1c2a9cf3ba1b591c61154b9402b45a01a9
Mike Caspar <mike@caspar.com>
2015-09-13 15:41:17 -0400

    set constants to final per findbugs

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/Iron/IronConstants.java
@@ -4,13 +4,13 @@ public final class IronConstants {
 
 
     public static final String DEF_PREFERRED_SERVER_SCHEME = "https" ;
-    public static String DEF_QUEUE_NAME = "Jenkins";
-    public static String DEFAULT_PREFERRED_SERVER_NAME
+    public static final String DEF_QUEUE_NAME = "Jenkins";
+    public static final String DEFAULT_PREFERRED_SERVER_NAME
             = "mq-rackspace-ord.iron.io";
     public static final int DEF_PREFERRED_SERVER_PORT = 443;
-    public static long DEF_EXPIRY_SEC = 806400L;
+    public static final long DEF_EXPIRY_SEC = 806400L;
 
-    public static long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
+    public static final long MILLISECONDS_TO_SECONDS_CONVERSION = 1000;
 
     /**
      * <p>Constructor for IronConstants.</p>
