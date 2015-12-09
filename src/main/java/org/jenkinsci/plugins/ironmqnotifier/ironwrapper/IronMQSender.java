cdd88bde589ad233fffbef703113ee60f02ece32
Mike Caspar <mike@caspar.com>
2015-12-09 13:38:44 -0500

    queue.push no longer sends anything other than body (the rest will default to queue settings)

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSender.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSender.java
@@ -13,50 +13,50 @@ public class IronMQSender {
         // placeholder for testing
     }
 
-    public void send ( final Client client,
-                       final IronMessageSettings ironMessageSettings)
+    public void send(final Client client,
+                     final IronMessageSettings ironMessageSettings)
             throws IOException, IllegalArgumentException {
 
-        checkMessageParameters (ironMessageSettings);
+        checkMessageParameters(ironMessageSettings);
 
-        Queue queue = client.queue (ironMessageSettings.getQueueName ());
+        Queue queue = client.queue(ironMessageSettings.getQueueName());
 
-        Message message = new Message ();
+        Message message = new Message();
 
-        IronMQMessage ironMQMessage = new IronMQMessage ();
+        IronMQMessage ironMQMessage = new IronMQMessage();
 
-        ironMQMessage.setBuildResult (ironMessageSettings.getBuildResultString ());
-        ironMQMessage.setJobName (ironMessageSettings.getJobName ());
+        ironMQMessage.setBuildResult(ironMessageSettings.getBuildResultString());
+        ironMQMessage.setJobName(ironMessageSettings.getJobName());
 
-        ironMQMessage.setExpirySeconds (ironMessageSettings.getExpirySeconds ());
+        ironMQMessage.setExpirySeconds(ironMessageSettings.getExpirySeconds());
 
 
-        message.setBody (ironMQMessage.toJson ());
+        message.setBody(ironMQMessage.toJson());
 
-        message.setExpiresIn (ironMessageSettings.getExpirySeconds ());
-        message.setBody (message.getBody ());
-
+        message.setExpiresIn(ironMessageSettings.getExpirySeconds());
+        message.setBody(message.getBody());
 
         String resultOfQueuePush
-                = queue.push (message.getBody(), 0, message.getExpiresIn()) ;
+                = queue.push(message.getBody());
 
-        if(resultOfQueuePush == null
-                || resultOfQueuePush.length () == 0) {
-            throw new IOException ("Not successful in sending message");
+
+        if (resultOfQueuePush == null
+                || resultOfQueuePush.length() == 0) {
+            throw new IOException("Not successful in sending message");
         }
     }
 
-    private void checkMessageParameters ( final IronMessageSettings ironMessageSettings)
+    private void checkMessageParameters(final IronMessageSettings ironMessageSettings)
             throws IllegalArgumentException {
 
-        if(ironMessageSettings.getExpirySeconds () == 0) {
-            throw new IllegalArgumentException ("expiry seconds Zero exception");
+        if (ironMessageSettings.getExpirySeconds() == 0) {
+            throw new IllegalArgumentException("expiry seconds Zero exception");
         }
 
-        String queueString = ironMessageSettings.getQueueName ();
+        String queueString = ironMessageSettings.getQueueName();
 
-        if(queueString == null || queueString.length () == 0) {
-            throw new IllegalArgumentException ("queueName is invalid exception");
+        if (queueString == null || queueString.length() == 0) {
+            throw new IllegalArgumentException("queueName is invalid exception");
         }
 
 
