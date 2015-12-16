9dd2a7a3ac65ffc59cbe8a4e4da8b427e12dbf52
Mike Caspar <mike@caspar.com>
2015-12-16 10:44:52 -0500

    remove unnecessary private constructor from IronConstants

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
@@ -2,9 +2,6 @@ package org.jenkinsci.plugins.ironmqnotifier.ironwrapper;
 
 public final class IronConstants {
 
-    private IronConstants() {
-    }
-
     public static final String DEF_PREFERRED_SERVER_SCHEME = "https";
     public static final String DEF_QUEUE_NAME = "Jenkins";
     public static final String DEFAULT_PREFERRED_SERVER_NAME
@@ -14,4 +11,6 @@ public final class IronConstants {
 
 
     public static final String DEF_MESSAGE_VERSION = "3.0";
+
+
 }
