b4ba9f3b98621a8766720005dbd73063fe4f065d
Mike Caspar <mike@caspar.com>
2016-06-17 20:12:11 -0400

    make constants non-static (bad practice)

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
@@ -2,13 +2,12 @@ package org.jenkinsci.plugins.ironmqnotifier.ironwrapper;
 
 public final class IronConstants {
 
-    public final String DEF_PREFERRED_SERVER_SCHEME = "https";
-    public final String DEF_QUEUE_NAME = "Jenkins";
-    public final String DEFAULT_PREFERRED_SERVER_NAME
-            = "mq-aws-us-east-1-1.iron.io";
-    public final int DEF_PREFERRED_SERVER_PORT = 443;
-    public final Long DEF_EXPIRY_SEC = 604800L;
-    public final String DEF_MESSAGE_VERSION = "3.0";
+    public String DEF_PREFERRED_SERVER_SCHEME = "https";
+    public String DEF_QUEUE_NAME = "Jenkins";
+    public String DEFAULT_PREFERRED_SERVER_NAME = "mq-aws-us-east-1-1.iron.io";
+    public int DEF_PREFERRED_SERVER_PORT = 443;
+    public Long DEF_EXPIRY_SEC = 604800L;
+    public String DEF_MESSAGE_VERSION = "3.0";
 
 
 
