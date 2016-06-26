2e2219123897634956d48612ea472bc5753a4e6e
Mike Caspar <mike@caspar.com>
2016-06-26 11:06:56 -0400

    test for empty string response from queue.push

--- src/test/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSenderTest.java
+++ src/test/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSenderTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -114,6 +115,31 @@ public class IronMQSenderTest {
 
     }
 
+    @Test(expected = IOException.class)
+    public void Sender_Queue_Push_Error_Empty_String() throws IOException {
+
+        IronMessageSettings mockIronMessageSettings = mock(IronMessageSettings.class);
+        when(mockIronMessageSettings.getExpirySeconds()).thenReturn(1000L);
+        when(mockIronMessageSettings.getQueueName()).thenReturn("test");
+
+        Queue mockQueue = mock(Queue.class);
+
+        when(mockQueue.getName()).thenReturn("test");
+        when(mockQueue.push(anyString())).thenReturn("");
+        when(mockQueue.push(anyString(),anyLong())).thenReturn("");
+
+        Client mockClient = mock(Client.class);
+        when(mockClient.queue(anyString())).thenReturn(mockQueue);
+
+
+        IronMQSender sender = new IronMQSender();
+
+
+        String resultOfPush = sender.send(mockClient, mockIronMessageSettings);
+
+        Assert.assertNotEquals("OK", resultOfPush);
+
+    }
 }
 
 
