32394967d8c2cf86aef4b00912599b06e9441b42
Mike Caspar <mike@caspar.com>
2019-10-24 07:51:38 -0700

    remove transient as this is causing problems with spotbugs

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
@@ -27,10 +27,10 @@ package org.jenkinsci.plugins.ironmqnotifier.ironwrapper;
 public final class IronConstants {
 
 
-    public final transient String DEF_QUEUE_NAME = "Jenkins";
-    public final transient String DEFAULT_PREFERRED_SERVER_NAME = "mq-aws-us-east-1-1.iron.io";
-    public final transient Long DEF_EXPIRY_SEC = 604800L;
-    public final transient String DEF_MESSAGE_VERSION = "3.0";
+    public final String DEF_QUEUE_NAME = "Jenkins";
+    public final String DEFAULT_PREFERRED_SERVER_NAME = "mq-aws-us-east-1-1.iron.io";
+    public final Long DEF_EXPIRY_SEC = 604800L;
+    public final String DEF_MESSAGE_VERSION = "3.0";
 
 
 }
