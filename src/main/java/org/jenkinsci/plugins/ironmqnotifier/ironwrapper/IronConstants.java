e72bf77a2d8dbb6a84d080f57a54512caa999f18
Mike Caspar <mike@caspar.com>
2019-10-23 22:23:20 -0700

    set IronConstants to transient to not have them stored in config files of jobs

--- src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
+++ src/main/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronConstants.java
@@ -27,12 +27,10 @@ package org.jenkinsci.plugins.ironmqnotifier.ironwrapper;
 public final class IronConstants {
 
 
-    public String DEF_QUEUE_NAME = "Jenkins";
-    public String DEFAULT_PREFERRED_SERVER_NAME = "mq-aws-us-east-1-1.iron.io";
-    public Long DEF_EXPIRY_SEC = 604800L;
-    public String DEF_MESSAGE_VERSION = "3.0";
-
-
+    public final transient String DEF_QUEUE_NAME = "Jenkins";
+    public final transient String DEFAULT_PREFERRED_SERVER_NAME = "mq-aws-us-east-1-1.iron.io";
+    public final transient Long DEF_EXPIRY_SEC = 604800L;
+    public final transient String DEF_MESSAGE_VERSION = "3.0";
 
 
 }
