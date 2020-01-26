1d7c03fd26f3a59b257d78255fd2f23afc5efa05
Mike Caspar <mike@caspar.com>
2020-01-25 16:06:52 -0800

    switch to mockito mathers.any

--- src/test/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSenderTest.java
+++ src/test/java/org/jenkinsci/plugins/ironmqnotifier/ironwrapper/IronMQSenderTest.java
@@ -31,7 +31,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
